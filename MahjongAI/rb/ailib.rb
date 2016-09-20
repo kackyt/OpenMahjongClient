b8ba840df3d4a621284a70008a6788975119c5d3
kackyt <t_kaki@nextappli.com>
2016-09-21 02:31:46 +0900

    [fix] テストコードを動かす

--- MahjongAI/rb/ailib.rb
+++ MahjongAI/rb/ailib.rb
@@ -3,6 +3,12 @@
 class Player
   attr_reader :tehai, :kawahai, :naki_mentsu
   attr_accessor :mentsu, :anpai, :kikenhai, :pai_kukan
+  
+  def initialize(tehai = [], kawahai = [], naki_mentsu = [])
+    @tehai = tehai
+    @kawahai = kawahai
+    @naki_mentsu = naki_mentsu
+  end
 end
 
 class Mentsu
@@ -38,7 +44,7 @@ class Mentsu
   def weight(pai_kukan)
     case @type
     when TYPE_SHUNTSU
-      return pai_kukan[@num] * pai_kukan[@num+1] * pai_kukan[@num+2]
+      return (pai_kukan[@num] * pai_kukan[@num+1] * pai_kukan[@num+2]).to_i
     when TYPE_KOUTSU
       return 4 if pai_kukan[@num] == 4
       return 1 if pai_kukan[@num] == 3
@@ -54,15 +60,24 @@ class Mentsu
   def self.all
     ret = []
     0.upto 8 do |n|
-      ret.push(Mentsu.new(TYPE_SHUNTSU, n)
-      ret.push(Mentsu.new(TYPE_SHUNTSU, n+9)
-      ret.push(Mentsu.new(TYPE_SHUNTSU, n+18)
+      ret.push(Mentsu.new(TYPE_SHUNTSU, n))
+      ret.push(Mentsu.new(TYPE_SHUNTSU, n+9))
+      ret.push(Mentsu.new(TYPE_SHUNTSU, n+18))
     end
     0.upto 33 do |n|
-      ret.push(Mentsu.new(TYPE_KOUTSU, n)
+      ret.push(Mentsu.new(TYPE_KOUTSU, n))
     end
     ret
   end
+  
+  def self.all_atama
+    ret = []
+    0.upto 33 do |n|
+      ret.push(Mentsu.new(TYPE_ATAMA, n))
+    end
+    ret
+  end
+  
   
   def sub(haiarray)
     case @type
@@ -81,8 +96,15 @@ class Mentsu
   
 end
 
-class hai
+class Pai
   attr_reader :num, :aka, :naki, :riichi
+  
+  def initialize(num=0, aka=false, naki = false, riichi = false)
+    @num = num
+    @aka = aka
+    @naki = naki
+    @riichi = riichi
+  end
   
   def getDoraHyouji
     if @num < 27
@@ -103,27 +125,27 @@ class hai
 end
 
 class Array
-  def sample
+  def w_sample
     self.size.times.map do |i|
       Array.new(yield(self[i]), self[i])
-    end.flatten.sample(n)
+    end.flatten.sample
   end
 end
 
 
 
 class MJ0
-  attr_reader :nokorihai
-  SIMU_SIZE=10000
+  SIMU_SIZE=5000
   
-  def simulate(players, doras)
-    @nokorihai = Array.new(34, 4.0)
-    @kikenhai = Array.new(34, 0.0)
+  def self.simulate(players, myself, doras)
     _nokorihai = Array.new(34, 4.0)
+    pai_kukan = Array.new(34, 0.0)
     
-    players.each do |player| 
+    (players + [myself]).each do |player| 
       player.anpai = Array.new(34, false)
       player.kikenhai = Array.new(34, 0.0)
+      player.tehai.each { |hai| _nokorihai[hai.num] -= 1 }
+      
       player.kawahai.each do |hai|
         _nokorihai[hai.num] -= 1 unless hai.naki
         player.anpai[hai.num] = true
@@ -132,39 +154,56 @@ class MJ0
       player.mentsu = []
     end
     
+    myself.kikenhai = Array.new(34, 0.0)
+    
     doras.each { |dora| _nokorihai[dora.getDoraHyouji] -= 1 }
     
     SIMU_SIZE.times do
-      players.each do |player|
-        player.pai_kukan = Array.new(_nokorihai)
+      (players + [myself]).each do |player|
+        player.pai_kukan = _nokorihai.clone
         # TODO 手読みで牌空間を変える
       end
       
-      until (subjects = players.select { |player| player.naki_mentsu.size + player.mentsu.size < 4 }).empty?
-      do
+      while !(subjects = players.select { |player| player.naki_mentsu.size + player.mentsu.size < 4 }).empty?
         subjects.each do |player|
-          mentsu = Mentsu.all.sample { |m| m.weight(player.pai_kukan) }
+          # メンツをランダムで抽出する
+          mentsu = Mentsu.all.w_sample { |m| m.weight(player.pai_kukan) }
           player.mentsu.push(mentsu)
-          players.each { |p2| mentsu.sub(p2.pai_kukan) }
-          mentsu.kiken(players.kikenhai)
+          # メンツを構成する牌を牌空間から除去する
+          (players + [myself]).each { |p2| mentsu.sub(p2.pai_kukan) }
+          # 危険度を更新する
+          mentsu.kiken(myself.kikenhai)
         end
       end
       
       players.each do |player|
+        # 頭をランダムで抽出する
+        mentsu = Mentsu.all_atama.w_sample { |m| m.weight(player.pai_kukan) }
+        player.mentsu.push(mentsu)
+        # メンツを構成する牌を牌空間から除去する
+        (players + [myself]).each { |p2| mentsu.sub(p2.pai_kukan) }
+        # 危険度を更新する
+        mentsu.kiken(myself.kikenhai)
       end
+    
+      pai_kukan.each_with_index { |pai, idx| pai_kukan[idx] += myself.pai_kukan[idx] }
     end
-      
-  end
-
-end
-
-
-
-
-
-module AILib
-  extend self
-  def get_tsumogiri
-    12
+    myself.pai_kukan = pai_kukan.map { |pai| pai / SIMU_SIZE }
+    myself.kikenhai.map! { |pai| pai / SIMU_SIZE }
   end
 end
+
+def p(num)
+  Pai.new(num)
+end
+
+def pais(a)
+  a.map { |n| Pai.new(n) }
+end
+
+enemies = [ Player.new, Player.new, Player.new ]
+player = Player.new(pais([0, 1, 2, 2, 3, 5, 7, 9, 14, 14, 15, 20, 23, 25]))
+
+MJ0.simulate(enemies, player, [p(15)])
+
+puts player.inspect
